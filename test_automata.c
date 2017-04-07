492d705cc5cf88b795e64f2398debcfee3c24ae4
sneaky-moose <alex.p00ka@gmail.com>
2017-04-07 15:27:32 +1000

    Change probability values to sensible model values

--- test_automata.c
+++ test_automata.c
@@ -1,22 +1,21 @@
 #include "c_automata.h"
 #include "arrays.h"
-#include "random.h"
 
 
 int main() 
 {
 	int i, j, N, *arr;
 	int types[4]; 
-	double probs[] = {0.00, 0.60, 0.05, 0.05, 0.05};
+	double probs[] = {0.00, 0.48, 0.1, 0.3, 0.1};
 	
 	N = 20;
 	
 	/* allocate 2D array & initialize values */
 	arr = arr_alloc(N * N);
 	
-	init_state(arr, N, 5); 
-	
-	iterate_display(arr, N, 100, probs, 0);
+	init_state(arr, N, 5);
+
+	iterate_display(arr, N, 100, probs, 1000000);
 	
 	automata_print(arr, N);
 	
